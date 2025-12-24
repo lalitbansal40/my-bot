0aa9f3d7fb2cd47a3b6186087d455d88eb3718b1
lalitbansal40 <165453425+lalitbansal40@users.noreply.github.com>
2025-12-24 03:03:15 +0000

    use serverless-http for express on lambda

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-import serverlessExpress from "@vendia/serverless-express";
-import app from "./app";
+import express from "express";
+import serverless from "serverless-http";
+import webhookRoutes from "./routes/webhook.route";
+import whatsappFlowRoutes from "./routes/whatsappFlow.route";
 
-export const handler = serverlessExpress({ app });
+const app = express();
+
+app.use(express.json());
+
+app.use("/webhook", webhookRoutes);
+app.use("/whatsappflow", whatsappFlowRoutes);
+
+app.get("/", (_req, res) => {
+  res.send(`<pre>Nothing to see here. Checkout README.md to start.</pre>`);
+});
+
+// Lambda handler
+export const serverHandler = serverless(app);
+export const handler = async (event:any, context:any) => {
+  return serverHandler(event, context);
+};
+// Optional (for local testing)
+export const server = app;
